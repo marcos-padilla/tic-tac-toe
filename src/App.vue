841591b4a4e521aa6745c7cec803f9e7647c3134
Marcos <padilladortamarcosalejandro@gmail.com>
2023-02-15 11:16:21 -0500

    Added store from vuex

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,6 @@ export default {
     data() {
         return {
             cells: Array(9).fill(null),
-            players: ['❌', '⭕']
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const store = createStore({
+	state() {
+		return {
+			cells: Array(9).fill(null),
+			players: ['❌', '⭕'],
+			currentPlayer: '❌',
+		}
+	},
+	mutations: {
+		changeTurn(state) {
+			if (state.currentPlayer == '❌') {
+				state.currentPlayer = '⭕'
+			} else {
+				state.currentPlayer = '❌'
+			}
+		},
+	},
+})
+
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
